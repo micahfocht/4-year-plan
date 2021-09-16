e93d17cc790d9f48c0082871f0102938922360e6
Johnson Olusegun <olusegun.johnson.oj@gmail.com>
2021-09-16 09:37:16 -0400

    bacground color fixed

--- src/pages/welcome_page.php
+++ src/pages/welcome_page.php
@@ -6,17 +6,20 @@
 <div class="welcome_main">
     <div class="container-fluid">
         <div class="row">
-            <div class="col welcome_main_left">
-                <img class="welcome_main_img" alt="logo" src="../assets/svg/bg.svg" />
-
-            </div>
-            <div class="col welcome_main_right_div">
+            <div class="col  welcome_main_left_div">
                 <h1>Welcome</h1>
                 <h2>Create Your Four</h2>
                 <h2>Year Plan</h2>
+
+
+            </div>
+            <div class="col welcome_main_right_div">
+                <!-- banner image -->
+                <img class="welcome_main_img" alt="logo" src="./assets/svg/bg.svg" />
             </div>
 
         </div>
+        <!-- end of first row -->
 
         <!-- start a new row -->
         <br />
@@ -28,45 +31,59 @@
                 <!-- content 1 -->
                 <div class="col">
 
-                    <div class="card welcome-card-image">
-                        <img src="../assets/svg/why.svg" class="card-img-top" alt="Why image">
+                    <div class="card welcome-card">
+                        <img src="./assets/svg/why.svg" class="card-img-top" alt="Why image">
                         <div class="card-body">
                             <h5 class="card-title">Why Four year plan</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of
-                                the card's content.</p>
+                            <div class="col-12 text-center">
+                                <a href="#" class="btn btn-outline-secondary btn-lg">Read
+                                    more</a>
+                            </div>
+
                         </div>
                     </div>
 
                 </div>
                 <!-- content 2 -->
                 <div class="col">
-                    <div class="card welcome-card-image">
-                        <img src="../assets/svg/benefits.svg" class="card-img-top" alt="Benefits">
+                    <div class="card welcome-card">
+                        <img src="./assets/svg/benefits.svg" class="card-img-top " alt="Benefits">
                         <div class="card-body">
-                            <h5 class="card-title">Benefits</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of
-                                the card's content.</p>
+                            <h5 class="card-title">Benefits this plan</h5>
+                            <div class="col-12 text-center">
+                                <a href="#" class="btn btn-outline-secondary btn-lg">Read
+                                    more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <!-- content 3 -->
-                <div class="col">
-                    <div class="card welcome-card-image">
-                        <img src="../assets/svg/team.svg" class="card-img-top" alt="Why image">
+                <div class="col ">
+                    <div class="card welcome-card">
+                        <img src="./assets/svg/team.svg" class="card-img-top" alt="Why image">
                         <div class="card-body">
                             <h5 class="card-title">Development Team</h5>
-                            <p class="card-text">names of developer, year </p>
+                            <div class="col-12 text-center">
+                                <a href="#" class="btn btn-outline-secondary btn-lg">Read
+                                    more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
 
             </div>
+            <!-- end of second row -->
+
+
         </div>
+
+        <!-- end of card view -->
+        <br />
+        <hr />
+
 
     </div>
 
-    <!-- end of card view -->
-    <hr>
+
+
 </div>
