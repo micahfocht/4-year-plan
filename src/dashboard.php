0a601b0a34434d3f027742a63bad7b63149f43ce
Johnson Olusegun <olusegun.johnson.oj@gmail.com>
2021-09-23 14:03:03 -0400

    Work on the conflict page

--- src/dashboard.php
+++ src/dashboard.php
@@ -5,4 +5,7 @@ include "./includes/redirect.php";
 include "./includes/header.php";
 include "./includes/main_nav_bar.php";
 
+
+// include footer section
+include './includes/footer_section.php';
 include "./includes/footer.php";
--- src/includes/footer_section.php
+++ src/includes/footer_section.php
@@ -1,3 +1,27 @@
 <div class="container-fluid footer-section">
+    <div class="footer-section__main-div">
+        <div class="row">
+            <div class="col footer-section__left-div">
 
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+
+                </ul>
+
+            </div>
+            <!-- empty section -->
+            <div class="col">
+                &nbsp;
+            </div>
+            <div class="col footer-section__right-div">
+                <p>Bethel University &copy; <?php echo date('Y'); ?></p>
+            </div>
+
+        </div>
+    </div>
 </div>
