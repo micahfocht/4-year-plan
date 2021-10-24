ec7ecf82491edcc6a90bfa08cc5d495bef343a8e
Micah Focht <bandmfocht@gmail.com>
2021-10-24 01:39:53 -0400

    Auth now checks if the email exists in the database.  We'll still need to handle passwords somehow, but this makes sure no the database has something to load since we know the user exists in the database.  It does take longer to load now and I'm not certain how to fix that if it's even possible.  Possibly store passwords in a different table and look there first for users to verify passwords and then fall back to the larger table to see if the user exists at all and then set their password.

--- src/auth.php
+++ src/auth.php
@@ -1,53 +1,50 @@
 <?php
-//require 'credentials.php';
+require './includes/creds.php';
 if (isset($_POST['email']) && isset($_POST['password'])) {
     // Create connection
     $email = $_POST["email"];
     $password = $_POST["password"];
     session_start();
     $result = true;
-    //$conn =  oci_connect('micahfocht','password','csoracle.betheluniversity.edu:1521');
-    //echo $conn;
-    /*$conn = new mysqli($servername, $dbuser, $dbpass, $dbname); //These credentials come from credentials.php
-    // Check connection
-    if ($conn->connect_error) {
-        die("Database offline");
-        //Because the database credentials and name are properly set, this error should never be displayed to the user
-        //except in cases of the database service not running, which is a bigger issue than this file needs to address.
-    }
-    $sql = "SELECT username, password, Name,User_ID FROM Users";
-    $result = $conn->query($sql);
-    */
+    $database =  oci_connect($user,$pass,'//csoracle.betheluniversity.edu:1521/orclpdb');
+    $query = oci_parse($database, "SELECT s_email from STUDENT");
+    oci_execute($query);
     if ($result){
-        /*if (mysqli_num_rows($result) >0){//If there are any entries in the database
-            $_SESSION["Status"] = "Incorrect Username or Password."; //Default the status to incorrect
-            while($user = mysqli_fetch_array($result)){ //While there are more rows
-                if($user['username'] == $username){//If the username matches
-                    if(password_verify($password, $user['password'])){//Check the password
-                        $_SESSION["Name"] = $user['Name'];//Set the session variables
-                        $_SESSION["Status"] = "Logged in";
-                        $_SESSION["UID"] = $user['User_ID'];
-                        $newsql = "INSERT INTO Access (Access_Time, User_ID) VALUES (CURRENT_TIMESTAMP, '" . $_SESSION["UID"] . "');";
-                        $conn->query($newsql);//Log the login in the database
-                        if(isset($_POST['remember'])) {//Check if the "remember" checkbox is checked and if it is, save the username in a cookie
-                            setcookie("username", $username, time() + 60 * 60 * 24 * 30);
-                        }else{
-                            setcookie("username", "", time() - 1);
-                        }
-                        header("Location: dashboard.php"); //redirect to the dashboard after login.
+        while($row = oci_fetch_array($query, OCI_ASSOC+OCI_RETURN_NULLS)){
+            foreach ($row as $item){
+                if($item == $email){
+                    $_SESSION["email"] = $email;//Set the session variables
+                    $_SESSION["Status"] = "Logged in";
+                    $_SESSION["UID"] = 0;
+                    if(isset($_POST['remember'])) {//Check if the "remember" checkbox is checked and if it is, save the username in a cookie
+                        setcookie("email", $email, time() + 60 * 60 * 24 * 30);
+                    }else{
+                        setcookie("email", "", time() - 1);
                     }
+                    header("Location: dashboard.php"); //redirect to the dashboard after login.
                 }
             }
-        }*/
-        $_SESSION["email"] = $email;//Set the session variables
-        $_SESSION["Status"] = "Logged in";
-        $_SESSION["UID"] = 0;
-        if(isset($_POST['remember'])) {//Check if the "remember" checkbox is checked and if it is, save the username in a cookie
-            setcookie("email", $email, time() + 60 * 60 * 24 * 30);
-        }else{
-            setcookie("email", "", time() - 1);
         }
-        header("Location: dashboard.php"); //redirect to the dashboard after login.
+            /*if (mysqli_num_rows($result) >0){//If there are any entries in the database
+                $_SESSION["Status"] = "Incorrect Username or Password."; //Default the status to incorrect
+                while($user = mysqli_fetch_array($result)){ //While there are more rows
+                    if($user['username'] == $username){//If the username matches
+                        if(password_verify($password, $user['password'])){//Check the password
+                            $_SESSION["Name"] = $user['Name'];//Set the session variables
+                            $_SESSION["Status"] = "Logged in";
+                            $_SESSION["UID"] = $user['User_ID'];
+                            $newsql = "INSERT INTO Access (Access_Time, User_ID) VALUES (CURRENT_TIMESTAMP, '" . $_SESSION["UID"] . "');";
+                            $conn->query($newsql);//Log the login in the database
+                            if(isset($_POST['remember'])) {//Check if the "remember" checkbox is checked and if it is, save the username in a cookie
+                                setcookie("username", $username, time() + 60 * 60 * 24 * 30);
+                            }else{
+                                setcookie("username", "", time() - 1);
+                            }
+                            header("Location: dashboard.php"); //redirect to the dashboard after login.
+                        }
+                    }
+                }
+            }*/
     }
 }
 include 'includes/redirect.php';
