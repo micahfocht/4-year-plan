ee065a7a341008e68c4539ff990eee2cb48fc63b
Micah Focht <bandmfocht@gmail.com>
2021-11-04 11:11:05 -0400

    Fix the other semesters pane

--- src/includes/othercourses.php
+++ src/includes/othercourses.php
@@ -1,18 +1,16 @@
 <?php
-$query = oci_parse($database, "select unique e.e_semester from ENROLL e, STUDENT s where e.e_stid = s.s_id and s.s_email = :email");
-oci_bind_by_name($query,":email",$email);
-oci_execute($query);
-while($code = oci_fetch_array($query, OCI_ASSOC+OCI_RETURN_NULLS)) {
-    $season = substr($code["E_SEMESTER"], -2);
-    echo($code["E_SEMESTER"]);
-    echo("<br>");
+$querysem = oci_parse($database, "select unique e.e_semester from ENROLL e, STUDENT s where e.e_stid = s.s_id and s.s_email = :email");
+oci_bind_by_name($querysem,":email",$email);
+oci_execute($querysem);
+while($semesters = oci_fetch_array($querysem, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    $season = substr($semesters["E_SEMESTER"], -2);
     if ($season != "SP" && $season != "FA") {
-        $query = oci_parse($database, "select unique e.e_seccode,e.e_secnum, c.c_title, c.c_max from ENROLL e, STUDENT s, COURSE c 
+        $query = oci_parse($database, "select unique e.e_seccode,e.e_secnum, c.c_title, c.c_max from ENROLL e, STUDENT s, COURSE c
                                                         where e.e_stid = s.s_id and s.s_email = :email and e.e_semester = :semester
                                                         and e.e_seccode = c.c_code and e.e_secnum = c.c_num and e.e_catalog = c.c_catalog
                                                         order by e.e_seccode, e.e_secnum");
         oci_bind_by_name($query, ":email", $email);
-        oci_bind_by_name($query, ":semester", $code['E_SEMESTER']);
+        oci_bind_by_name($query, ":semester", $semesters['E_SEMESTER']);
         oci_execute($query);
         while ($code = oci_fetch_array($query, OCI_ASSOC + OCI_RETURN_NULLS)) {
             echo('
@@ -49,30 +47,30 @@ while($code = oci_fetch_array($query, OCI_ASSOC+OCI_RETURN_NULLS)) {
         oci_bind_by_name($query, ":email", $email);
         oci_bind_by_name($query, ":semester", $semester);
         oci_execute($query);
-        while ($code = oci_fetch_array($query, OCI_ASSOC + OCI_RETURN_NULLS)) {
+        while ($codemanual = oci_fetch_array($query, OCI_ASSOC + OCI_RETURN_NULLS)) {
             echo('
                                         <!-- COL 2 ROW -->
                                         <div class="col-2">
                                         <p>' .
-                $code["E_SECCODE"] .
+                $codemanual["E_SECCODE"] .
                 '</p>
                                         </div>
                                         <!-- Course Code -->
                                         <div class="col-2">
                                         <p>' .
-                $code["E_SECNUM"] .
+                $codemanual["E_SECNUM"] .
                 '</p>
                                         </div>
                                         <!-- Course Name -->
                                         <div class="col-7">
                                         <p>' .
-                $code["E_TITLE"] .
+                $codemanual["E_TITLE"] .
                 '</p>
                                         </div>
                                         <!-- hours -->
                                         <div class="col-1">
                                         <p>' .
-                $code["E_CREDIT"] .
+                $codemanual["E_CREDIT"] .
                 '</p>
                                         </div>
                                         '
