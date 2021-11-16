91ce26e122605d6e89ea7a1e64585f438f27aad4
Tianna White <tiannawhite@Tiannas-MacBook-Pro.local>
2021-11-16 15:50:36 -0500

    logic for the textbox

--- src/includes/courseselect.php
+++ src/includes/courseselect.php
@@ -41,6 +41,7 @@ include "./includes/codelookup.php";
 </div>
 </form>
     <div class="col-5">
+        <div class="autocomplete"
         <button id="<?php
         echo "submit" . $semester;
         ?>" class="btn btn-outline-primary btn-lg">Add</button>
@@ -80,4 +81,78 @@ include "./includes/codelookup.php";
 
 
     });
+
+</script>
+<script>
+    function autocomplete(inp,arr) {
+        var currentFocus;
+        inp.addEventListener("input", function(e) {
+            var j, k, i, v = this.value;
+            closeAllLists();
+            if(!v) {
+                return false;
+            }
+            currentFocus = -1;
+            j = document.createElement("DIV");
+            j.setAttribute("id", this.id + "autocomplete-list");
+            j.setAtrribute("class","autocomplete-items");
+            this.parentNode.appendChild(j);
+            for(i=0; i<arr.length; i++) {
+                if (arr[i].substr(0, v.length).toUpperCase() == v.toUpperCase()) {
+                    k = document.createElement("DIV");
+                    k.innerHTML = "<strong>" + arr[i].substr(0, v.length) + "</strong>";
+                    k.innerHTML += "<input type = 'hidden' value = '" + arr[i] + "'>";
+                    k.addEventListener("click", function (e) {
+                        inp.value = this.getElementsByClassName("input") [0].value;
+                        closeAllLists();
+
+                    });
+                    j.appendChild(k);
+
+                }
+            }
+        });
+        inp.addEventListener("keydown",function(e){
+            var x = document.getElementById(this.id + "autocomplete-list");
+            <!-- removed x tag name-->
+            if(e.keyCode == 40) {
+                currentfocus++;
+                addActive(x);
+            } else if (e.keyCode == 38) {
+                currentfocus--;
+                addActive(x);
+            } else if (e.keyCode == 13) {
+                e.preventDefault();
+                if (currentFocus > -1) {
+                    x[currentFocus].click();
+                }
+            }
+        });
+        function addActive(x) {
+            if (!x)
+                return false;
+            removeActive(x);
+            if (currentFocus >= x.length)
+                curentFocus = 0;
+            if (currentFocus < 0)
+                currentFocus = (x.length -1);
+            x[currentFocus].classList.add("autocomplete-active");
+        }
+        function removeActive(x) {
+            for ( var i = 0; i < x.length; i++) {
+                x[i].classList.remove("autocomplete-active");
+            }
+        }
+        function closeAllLists(elmnt) {
+            var x = document.getElementsByClassName("autocomplete-items");
+            for( var i = 0; i < x.length; i++) {
+                if (elmnt != x[i] && elmnt != inp) {
+                    x[i].parentNode.removeChild(x[i]);
+                }
+            }
+        }
+        document.addEventListener("click", function(e) {
+            closeAllLists(e.target))
+        });
+    }
 </script>
