f55db804f23a07a1296fe5ec68cc5038e799e902
Johnson Olusegun <olusegun.johnson.oj@gmail.com>
2021-12-02 14:23:17 -0500

    documentation

--- src/dashboard.php
+++ src/dashboard.php
@@ -11,137 +11,126 @@ $email = $_SESSION["email"];
 $database = oci_connect($user,$pass,'//csoracle.betheluniversity.edu:1521/orclpdb');
 ?>
 
+<!-- general container div -->
+<div class="general-container">
+    <!-- container fluid and dashboard div -->
+    <div class="container-fluid dashboard">
 
-    <div class="general-container">
-        <div class="container-fluid dashboard">
-
-
-            <?php
+        <?php
             include "./includes/stuinfo.php";
             ?>
 
 
-<!--            end of heading row-->
+        <!-- heading row-->
 
-<!--            #####################################################-->
-<div class="row">
-
+        <div class="row">
             <!-- content 1 -->
-                <div class="col-6">
-                    <div class="card reusable-card">
-                        <div class="card-body">
-                            <h5 class="card-title">Fall 2019</h5>
-                            <div class="row">
-                                <?php
-                                    $semester = '2019FA';
-                                    include "./includes/coursespast.php";
+            <div class="col-6">
+                <div class="card reusable-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Fall 2019</h5>
+                        <div class="row">
+                            <?php
+                                $semester = '2019FA';
+                                include "./includes/coursespast.php";
                                 ?>
-                        </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
 
-    <div class="col-6">
-        <div class="card reusable-card">
-            <div class="card-body">
-                <h5 class="card-title">Spring 2020</h5>
-                <div class="row">
-                    <?php
+            <div class="col-6">
+                <div class="card reusable-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Spring 2020</h5>
+                        <div class="row">
+                            <?php
                     $semester = '2020SP';
                     include "./includes/coursespast.php";
                     ?>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        <!-- end of heading row -->
 
-
-</div>
-            <div class="row">
-
-                <!-- content 1 -->
-                <div class="col-6">
-                    <div class="card reusable-card">
-                        <div class="card-body">
-                            <h5 class="card-title">Fall 2020</h5>
-                            <div class="row">
-                                <?php
+        <!-- start of second row -->
+        <div class="row">
+            <!-- content 1 -->
+            <div class="col-6">
+                <div class="card reusable-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Fall 2020</h5>
+                        <div class="row">
+                            <?php
                                 $semester = '2020FA';
                                 include "./includes/coursespast.php";
                                 ?>
-                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <!-- end of col - 6 -->
 
 
-                <div class="col-6">
-                    <div class="card reusable-card">
-                        <div class="card-body">
-                            <h5 class="card-title">Spring 2021</h5>
-                            <div class="row">
-                                <?php
+            <div class="col-6">
+                <div class="card reusable-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Spring 2021</h5>
+                        <div class="row">
+                            <?php
                                 $semester = '2021SP';
                                 include "./includes/coursespast.php";
                                 ?>
-                            </div>
                         </div>
                     </div>
                 </div>
-
-
             </div>
-            <div class="row">
+            <!-- end of second col - 6 -->
+        </div>
+        <!-- end of second row -->
 
-                <!-- content 1 -->
-                <div class="col-6">
-                    <div class="card reusable-card">
-                        <div class="card-body">
-                            <h5 class="card-title">Fall 2021</h5>
-                            <div class="row">
-                                <?php
+        <!-- start of third row -->
+        <div class="row">
+            <!-- content 1 -->
+            <div class="col-6">
+                <div class="card reusable-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Fall 2021</h5>
+                        <div class="row">
+                            <?php
                                 $semester = '2021FA';
                                 include "./includes/coursespast.php";
                                 ?>
-                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
 
-                <div class="col-6">
-                    <div class="card reusable-card">
-                        <div class="card-body">
-                            <h5 class="card-title">Spring 2022</h5>
-                            <div class="row">
-                                <?php
+            <div class="col-6">
+                <div class="card reusable-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Spring 2022</h5>
+                        <div class="row">
+                            <?php
                                 $semester = '2022SP';
                                 include "./includes/coursespast.php";
                                 include "./includes/courseselect.php";
                                 ?>
-                            </div>
                         </div>
                     </div>
                 </div>
-                <?php
+            </div>
+            <!-- end of row -->
+            <?php
                 include "./includes/othercourses.php";
                 ?>
-
-              <!--  <div class="col-2">
-                    <div class="card welcome-card">
-                        <div class="card-body">
-                            <button type="button" class="btn btn-outline-primary btn-lg">Add</button>
-                        </div>
-                    </div>
-                </div>
-            </div>   -->
-<!--    end of welcome card-->
-
-
-        </div>
         </div>
     </div>
+</div>
 
 <?php
 include "./includes/footer_section.php";
--- src/preferences.php
+++ src/preferences.php
@@ -26,23 +26,23 @@ $database = oci_connect($user,$pass,'//csoracle.betheluniversity.edu:1521/orclpd
                         <h5 class="card-title">Expected Graduation Semester</h5>
                         <div class="row">
                             <form action="./includes/updategradsemester.php" method="post">
-                            <div class="col-3">
-                                <label for="year" class="formgray">Graduation year:</label>
-                                <select name="year" id="year">
-                                    <option value="null">Year</option>
-                                    <option value="2022">2022</option>
-                                    <option value="2023">2023</option>
-                                    <option value="2024">2024</option>
-                                </select>
-                            </div>
-                            <div class="col-3">
-                                <label for="semester" class="formgray">Graduation year:</label>
-                                <select name="semester" id="semester">
-                                    <option value="null">Semester</option>
-                                    <option value="FA">FA</option>
-                                    <option value="SP">SP</option>
-                                </select>
-                            </div>
+                                <div class="col-3">
+                                    <label for="year" class="formgray">Graduation year:</label>
+                                    <select name="year" id="year">
+                                        <option value="null">Year</option>
+                                        <option value="2022">2022</option>
+                                        <option value="2023">2023</option>
+                                        <option value="2024">2024</option>
+                                    </select>
+                                </div>
+                                <div class="col-3">
+                                    <label for="semester" class="formgray">Graduation year:</label>
+                                    <select name="semester" id="semester">
+                                        <option value="null">Semester</option>
+                                        <option value="FA">FA</option>
+                                        <option value="SP">SP</option>
+                                    </select>
+                                </div>
                                 <div class="col-6">
                                     <input type="submit" value="Submit">
                                 </div>
@@ -55,37 +55,37 @@ $database = oci_connect($user,$pass,'//csoracle.betheluniversity.edu:1521/orclpd
         <div class="row">
 
             <!-- content 2 -->
-            <div class="col-12">
+            <div class="container-fluid">
                 <div class="card reusable-card">
                     <div class="card-body">
                         <h5 class="card-title">Manually Enter Courses</h5>
                         <div class="row">
                             <h3>Manually add courses that have been taken in the past if they are not available.</h3>
-                                <form action="./includes/addcourse.php" method="post" id="addcourse">
-                                    <div class="col-2">
-                                        <label for="seccode" class="formgray">Section Code</label>
-                                        <input type="text" name="seccode" id="seccode">
-                                    </div>
-                                    <div class="col-1">
-                                        <label for="secnum" class="formgray">Section Number</label>
-                                        <input type="text" name="secnum" id="secnum">
-                                    </div>
-                                    <div class="col-3">
-                                        <label for="semester" class="formgray">Semester</label>
-                                        <input type="text" name="semester" id="semester">
-                                    </div>
-                                    <div class="col-3">
-                                        <label for="coursetitle" class="formgray">Title</label>
-                                        <input type="text" name="coursetitle" id="coursetitle">
-                                    </div>
-                                    <div class="col-1">
-                                        <label for="credit" class="formgray">Credit</label>
-                                        <input type="text" name="credit" id="credit">
-                                    </div>
-                                    <div class="col-2">
-                                        <input type="submit" value="Submit">
-                                    </div>
-                                </form>
+                            <form action="./includes/addcourse.php" method="post" id="addcourse">
+                                <div class="col">
+                                    <label for="seccode" class="formgray">Section Code</label>
+                                    <input type="text" name="seccode" id="seccode" size='4'>
+                                </div>
+                                <div class="col">
+                                    <label for="secnum" class="formgray">Section Number</label>
+                                    <input type="text" name="secnum" id="secnum" size='4'>
+                                </div>
+                                <div class="col">
+                                    <label for="semester" class="formgray">Semester</label>
+                                    <input type="text" name="semester" id="semester" size='4'>
+                                </div>
+                                <div class="col">
+                                    <label for="coursetitle" class="formgray">Title</label>
+                                    <input type="text" name="coursetitle" id="coursetitle" size='4'>
+                                </div>
+                                <div class="col">
+                                    <label for="credit" class="formgray">Credit</label>
+                                    <input type="text" name="credit" id="credit" size='4'>
+                                </div>
+                                <div class="col">
+                                    <input type="submit" value="Submit">
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
