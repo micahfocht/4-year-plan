c1fe2a6e2d2588c4bcbf26ca12ed7528ac393c5c
Micah Focht <micah@focht.xyz>
2022-10-12 16:35:27 +0000

    Got the basics working, loads all classes.

--- src/test.php
+++ src/test.php
@@ -12,7 +12,7 @@
         $email = 'micah.focht@betheluniversity.edu';
         $database = oci_connect($user,$pass,'//csoracle.betheluniversity.edu:1521/orclpdb');
 
-        $query = oci_parse($database, "select * from STUDENT s where s.s_email = :email");
+        $query = oci_parse($database, "select s_id from STUDENT s where s.s_email = :email");
         oci_bind_by_name($query,":email", $email); //Get the name
         oci_execute($query);
         while ($row = oci_fetch_array($query, OCI_ASSOC)) {
@@ -32,10 +32,19 @@
         ?>
             </ul>
         </div>
-        <?php
-            $currsem = "2019FA";
-            include './includes/semester.php';
-        ?>
+        <div class="row">
+            <?php
+                $query = oci_parse($database, "select unique e_semester from enroll e, student s where s.s_email = :email and e.e_stid = s.s_id order by regexp_replace(e.e_semester, '[^0-9]+', ''), case regexp_replace(e.e_semester, '[^a-zA-Z]+', '') WHEN 'SP' THEN 1 WHEN 'SU' THEN 2 WHEN 'FA' THEN 3 WHEN 'WI' THEN 4 END");
+                #$query = oci_parse($database, "select unique e_semester from enroll");
+                oci_bind_by_name($query,":email", $email);
+                oci_execute($query);
+                oci_fetch_all($query,$row);
+                foreach ($row['E_SEMESTER'] as $item){
+                    $currsem = $item;
+                    include './includes/semester.php';
+                }
+            ?>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     </body>
 </html>
